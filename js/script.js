1eafc2c0c9867f726d82439d66f19414da99b188
ahmed806 <ahmdmslm806@gmail.com>
2025-02-24 16:20:36 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var btnalert = document.querySelectorAll(".alert-page"); 
+btnalert.forEach(function(alert123){
+    alert123.onclick = function(){
+        alert("copied to clipboard");
+    }
+})
+// //////////////////////////////////////////////////////////////
